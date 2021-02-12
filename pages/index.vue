acb452fa4dfe4ccbc370fc41bf300de3b4f9bc06
Hrach-1 <steepgaskin@gmail.com>
2021-02-12 17:48:57 +0400

    db json

--- pages/index.vue
+++ pages/index.vue
@@ -1,9 +1,39 @@
 <template>
   <div>
     <h1>Events</h1>
+
+    <event-card
+      v-for="(evnet, index) in evnets"
+      :key="index"
+      :event="event"
+      :data-index="index"
+    ></event-card>
   </div>
 </template>
 
 <script>
-export default {}
+import EventCard from '../components/EventCard.vue'
+export default {
+  components: { EventCard },
+  head() {
+    return {
+      title: 'Event Listing'
+    }
+  },
+  asyncData({ $axios, error }) {
+    return $axios
+      .get('http://localhost:3000/events')
+      .then(response => {
+        return {
+          events: response.data
+        }
+      })
+      .catch(e => {
+        error({
+          statusCode: 503,
+          message: 'Unable to fetch events at this time. Please try again.'
+        })
+      })
+  }
+}
 </script>
